a572d31d35283af2bfa21093d0b39fbeb5e4bb6e
Stevan1707 <1553868699@qq.com>
2025-10-15 16:30:54 +0800

    部署测试

---
+++ back-end/src/main/java/org/star5025/backend/controller/testcontroller/HelloController.java
@@ -0,0 +1,18 @@
+package org.star5025.backend.controller.testcontroller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@CrossOrigin
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+
+        return "Hello World";
+    }
+}
