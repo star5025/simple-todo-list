48b120d73f884f6f3bc0d10ae87ee4832bb21ac3
ZidaWang <starzd5025@gmail.com>
2025-10-15 14:46:28 +0800

    Initialize back-end framework

---
+++ back-end/src/test/java/org/star5025/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.star5025.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
