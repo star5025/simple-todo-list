de47f51f96d003cf077c096662dfa3d5fad67068
Stevan1707 <1553868699@qq.com>
2025-10-25 14:50:41 +0800

    feat: add more information for swagger Api document

--- back-end/src/main/java/org/star5025/backend/config/WebMvcConfig.java
+++ back-end/src/main/java/org/star5025/backend/config/WebMvcConfig.java
@@ -1,10 +1,18 @@
 package org.star5025.backend.config;
 
+import io.swagger.annotations.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.star5025.backend.interceptor.JwtTokenInterceptor;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -16,5 +24,26 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtTokenInterceptor)
                 .addPathPatterns("/api/**") // 需要拦截的路径（如所有/api下的接口）
                 .excludePathPatterns("/api/user/login", "/api/user/register"); // 排除登录/注册接口
+    }
+
+    /**
+     * 通过knife4j生成接口文档
+     * @return
+     */
+    @Bean
+    public Docket docket(){
+
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("Simple-todo-list接口文档")
+                .version("1.0")
+                .description("Simple-todo-list接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.star5025.backend.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
     }
 }
