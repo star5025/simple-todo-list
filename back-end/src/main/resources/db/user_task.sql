f39c6f418dc2769b8f6ffea3ef9d1c217f213f19
ZidaWang <starzd5025@gmail.com>
2025-10-28 23:45:20 +0800

    fix: user_task中间表的主键同时设置为外键，并添加级联删除属性

--- back-end/src/main/resources/db/user_task.sql
+++ back-end/src/main/resources/db/user_task.sql
@@ -3,7 +3,9 @@ create table user_task
     user_id     varchar(255) not null,
     task_id     varchar(255) not null,
     star_status tinyint(1)   null comment '星标状态',
-    primary key (user_id, task_id)
+    primary key (user_id, task_id),
+    foreign key (user_id) references user(user_id) on delete cascade,
+    foreign key (task_id) references task(task_id) on delete cascade
 )
     comment '中间表';
 
